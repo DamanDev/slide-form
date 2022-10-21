14f0986b3f36daac32ffc16d42ab43102dc9ca85
daman <daman8dev@gmail.com>
2022-10-21 12:56:41 +1100

    First push

---
+++ ExApi/DbHelper/ConnectionDb.js
@@ -0,0 +1,9 @@
+const {MongoClient} = require("mongodb");
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+module.exports = {
+     client : client,
+     db : client.db('myDb')
+}
+ 
